3dbfc8d35090c69bdff53543a49329f699e062b5
RexAshh <95988728+Rexashh@users.noreply.github.com>
2023-02-09 15:57:51 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "6054586380:AAGh0oYvdxUU1cI13JHcj_UG4RIZq4AOJZs")
-API_ID = int(os.environ.get("API_ID", ""))
-API_HASH = os.environ.get("API_HASH", "")
+API_ID = int(os.environ.get("API_ID", "9143931"))
+API_HASH = os.environ.get("API_HASH", "5dcaf3b395762de3172651527214da4d")
 LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001746351071"))
 AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "2012224978").split())
 DB_URL = os.environ.get("DB_URL", "mongodb+srv://rexa:rexa@rexa.lyguuwj.mongodb.net/?retryWrites=true&w=majority")
