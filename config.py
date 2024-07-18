6b61a10488104ebcb0828b9890f9abe91e10b74a
antoo69 <antobarry69@gmail.com>
2024-07-18 18:47:40 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "6054586380:AAGh0oYvdxUU1cI13JHcj_UG4RIZq4AOJZs")
-API_ID = int(os.environ.get("API_ID", "9143931"))
-API_HASH = os.environ.get("API_HASH", "5dcaf3b395762de3172651527214da4d")
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001746351071"))
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7285247985:AAGJ6fWfl3VUDgvc2PxG-q4Pz0unLpZ2_yw")
+API_ID = int(os.environ.get("API_ID", "28518520"))
+API_HASH = os.environ.get("API_HASH", "c858cde56cb2b2050a64df7e65de567b")
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002236001760"))
 AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "2012224978").split())
-DB_URL = os.environ.get("DB_URL", "mongodb+srv://rexa:rexa@rexa.lyguuwj.mongodb.net/?retryWrites=true&w=majority")
-DB_NAME = os.environ.get("DB_NAME", "rexa")
+DB_URL = os.environ.get("DB_URL", "mongodb+srv://kcici715:buburayam1@cluster0.clpcyju.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DB_NAME = os.environ.get("DB_NAME", "kcici715")
 BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", True))
