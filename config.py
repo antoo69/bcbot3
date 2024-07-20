2976bff1fefce10c058e871874ea83f9cd78badb
antoo69 <antobarry69@gmail.com>
2024-07-20 11:00:17 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import os
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7077715497:AAEVo6y08ucwy3XgDh9zVKtGvBwb8zFFTk4")
-API_ID = int(os.environ.get("API_ID", "28518520"))
-API_HASH = os.environ.get("API_HASH", "c858cde56cb2b2050a64df7e65de567b")
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002236001760"))
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7254722365:AAEkvVBHV1kE-yKyj3RIGlzXdYN9C4-_H-c")
+API_ID = int(os.environ.get("API_ID", "29305295"))
+API_HASH = os.environ.get("API_HASH", "6838cc67172f18fe5f302c158ce2fbfa")
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002079962899"))
 AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "2012224978").split())
 DB_URL = os.environ.get("DB_URL", "mongodb+srv://kcici715:buburayam1@cluster0.clpcyju.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DB_NAME = os.environ.get("DB_NAME", "kcici715")
